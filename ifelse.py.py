cfd40637868ad61b02f9f457dbef97eb1bde12a1
kimkangeun00 <kimkangeun77@naver.com>
2025-03-28 13:56:08 +0900

    3.

---
+++ ifelse.py.py
@@ -0,0 +1,11 @@
+sel = int(input('1.입력한 수식 계산 2. 두 수 사이의 합계: '))
+if sel == 1:
+    num = input("수식을 입력하세요: ")
+    result = eval(num)
+    print(f"{num} 결과는 {result}입니다.")
+
+else:
+    num1 = input("첫 번쨰 숫자를 입력하세요:")
+    num2 = input("두 번쨰 숫자를 입력하세요:")
+    result = num1 + num2
+    print(f"{num1}+...+{num2}는 {result}입니다.")
